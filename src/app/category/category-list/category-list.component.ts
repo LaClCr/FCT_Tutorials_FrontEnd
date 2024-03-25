81691605c85941909f96b0e282656695c67c39cf
Clemente Crespo external <laia.clemente-crespo.external@capgemini.com>
2024-03-25 12:37:12 +0100

    Listado simple -  Añadiendo acciones edit y create

---
+++ src/app/category/category-list/category-list.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { Category } from '../model/Category';
+import { CategoryService } from '../category.service';
+
+@Component({
+  selector: 'app-category-list',
+  templateUrl: './category-list.component.html',
+  styleUrls: ['./category-list.component.scss']
+})
+export class CategoryListComponent implements OnInit {
+
+  dataSource = new MatTableDataSource<Category>();
+  displayedColumns: string[] = ['id', 'name', 'action'];
+
+  constructor(
+    private categoryService: CategoryService,
+  ) { }
+
+  ngOnInit(): void {
+    this.categoryService.getCategories().subscribe(
+      categories => this.dataSource.data = categories
+    );
+  }
+}
---
+++ src/app/category/category.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatTableModule } from '@angular/material/table';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { CategoryListComponent } from './category-list/category-list.component';
+
+@NgModule({
+  declarations: [CategoryListComponent],
+  imports: [
+    CommonModule,
+    MatTableModule,
+    MatIconModule, 
+    MatButtonModule
+  ],
+})
+export class CategoryModule { }
